7fcc3b167be867142371178d52bcb60c87ee7682
chth <chth@nus.edu.sg>
2022-08-29 17:09:17 +0800

    Encapsulate

--- AnonymousPipeClient/AnonymousPipeClient.cs
+++ AnonymousPipeClient/AnonymousPipeClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.IO.Pipes;
 using System.Linq;
 using System.Text;
@@ -18,29 +19,34 @@ namespace TestPipe
                     Console.WriteLine("[CLIENT] Current TransmissionMode: {0}.",
                        pipeClient.TransmissionMode);
 
-                    using (StreamReader sr = new StreamReader(pipeClient))
-                    {
-                        // Display the read text to the console
-                        string temp;
-
-                        // Wait for 'sync message' from the server.
-                        do
-                        {
-                            Console.WriteLine("[CLIENT] Wait for sync...");
-                            temp = sr.ReadLine();
-                        }
-                        while (!temp.StartsWith("SYNC"));
-
-                        // Read the server data and echo to the console.
-                        while ((temp = sr.ReadLine()) != null)
-                        {
-                            Console.WriteLine("[CLIENT] Echo: " + temp);
-                        }
-                    }
+                    Receive(pipeClient);
                 }
             }
             Console.Write("[CLIENT] Press Enter to continue...");
             Console.ReadLine();
+        }
+
+        static void Receive(PipeStream pipeStream)
+        {
+            using (StreamReader sr = new StreamReader(pipeStream))
+            {
+                // Display the read text to the console
+                string temp;
+
+                // Wait for 'sync message' from the server.
+                do
+                {
+                    Console.WriteLine("[CLIENT] Wait for sync...");
+                    temp = sr.ReadLine();
+                }
+                while (!temp.StartsWith("SYNC"));
+
+                // Read the server data and echo to the console.
+                while ((temp = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine("[CLIENT] Echo: " + temp);
+                }
+            }
         }
     }
 }
--- AnonymousPipeServer/AnonymousPipeServer.cs
+++ AnonymousPipeServer/AnonymousPipeServer.cs
@@ -28,31 +28,36 @@ namespace TestPipe
 
                 pipeServer.DisposeLocalCopyOfClientHandle();
 
-                try
-                {
-                    // Read user input and send that to the client process.
-                    using (StreamWriter sw = new StreamWriter(pipeServer))
-                    {
-                        sw.AutoFlush = true;
-                        // Send a 'sync message' and wait for client to receive it.
-                        sw.WriteLine("SYNC");
-                        pipeServer.WaitForPipeDrain();
-                        // Send the console input to the client process.
-                        Console.Write("[SERVER] Enter text: ");
-                        sw.WriteLine(Console.ReadLine());
-                    }
-                }
-                // Catch the IOException that is raised if the pipe is broken
-                // or disconnected.
-                catch (IOException e)
-                {
-                    Console.WriteLine("[SERVER] Error: {0}", e.Message);
-                }
+                Send(pipeServer);
             }
 
             pipeClient.WaitForExit();
             pipeClient.Close();
             Console.WriteLine("[SERVER] Client quit. Server terminating.");
+        }
+
+        static void Send(AnonymousPipeServerStream anonymousPipeServerStream)
+        {
+            try
+            {
+                // Read user input and send that to the client process.
+                using (StreamWriter sw = new StreamWriter(anonymousPipeServerStream))
+                {
+                    sw.AutoFlush = true;
+                    // Send a 'sync message' and wait for client to receive it.
+                    sw.WriteLine("SYNC");
+                    anonymousPipeServerStream.WaitForPipeDrain();
+                    // Send the console input to the client process.
+                    Console.Write("[SERVER] Enter text: ");
+                    sw.WriteLine(Console.ReadLine());
+                }
+            }
+            // Catch the IOException that is raised if the pipe is broken
+            // or disconnected.
+            catch (IOException e)
+            {
+                Console.WriteLine("[SERVER] Error: {0}", e.Message);
+            }
         }
     }
 }
