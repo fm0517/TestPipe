edcf5ad347c2c6497961fcfbf871b8b4fead2345
chth <chth@nus.edu.sg>
2022-08-30 15:00:32 +0800

    remove unnecessary using

--- AnonymousPipeClient/AnonymousPipeClient.cs
+++ AnonymousPipeClient/AnonymousPipeClient.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.IO.Pipes;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO.Pipes;
 
 namespace TestPipe
 {
