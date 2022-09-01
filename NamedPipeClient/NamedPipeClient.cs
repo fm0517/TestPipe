f900640523ef142352118caee24c88eb990c5706
chth <chth@nus.edu.sg>
2022-09-01 14:35:54 +0800

    Server send cmd to client, client execute cmd, and then return log file to server

--- NamedPipeClient/NamedPipeClient.cs
+++ NamedPipeClient/NamedPipeClient.cs
@@ -2,140 +2,193 @@
 using System.Diagnostics;
 using System.IO;
 using System.IO.Pipes;
+using System.Net.Sockets;
+using System.Net;
 using System.Security.Principal;
 using System.Text;
 using System.Threading;
 
-public class PipeClient
+namespace TestPipe
 {
-    private static int numClients = 4;
-
-    public static void Main(string[] args)
+    public class PipeClient
     {
-        if (args.Length > 0)
+        private static int numClients = 4;
+
+        public static void Main(string[] args)
         {
-            if (args[0] == "spawnclient")
+            if (args.Length > 0)
             {
-                var pipeClient = new NamedPipeClientStream(".", "testpipe", PipeDirection.InOut, PipeOptions.None, TokenImpersonationLevel.Impersonation);
-
-                Console.WriteLine("Connecting to server ...\n");
-                pipeClient.Connect();
-
-                var ss = new StreamString(pipeClient);
-                // Validate the server's signature string.
-                if (ss.ReadString() == "I am the one true server!")
+                if (args[0] == "spawnclient")
                 {
-                    // The client security token is sent with the first write.
-                    // Send the name of the file whose contents are returned
-                    // by the server.
-                    ss.WriteString($"c:/textfile{args[1]}.txt");
+                    string clientId = args[1];
+                    int threadId = Thread.CurrentThread.ManagedThreadId;
+                    Console.WriteLine($"This is client-{clientId}, ThreadId = {threadId}");
+                    var pipeClient = new NamedPipeClientStream(".", "testpipe", PipeDirection.InOut, PipeOptions.None, TokenImpersonationLevel.Impersonation);
 
-                    // Print the file to the screen.
-                    Console.Write(ss.ReadString());
-                }
-                else
-                {
-                    Console.WriteLine("Server could not be verified.");
-                }
-                pipeClient.Close();
-                // Give the client process some time to display results before exiting.
-                Thread.Sleep(4000);
-            }
-        }
-        else
-        {
-            Console.WriteLine("\n*** Named pipe client stream with impersonation example ***\n");
-            StartClients();
-        }
-    }
+                    Console.WriteLine("Connecting to server ...\n");
+                    pipeClient.Connect();
 
-    // Helper function to create pipe client processes
-    private static void StartClients()
-    {
-        string currentProcessName = Environment.CommandLine;
+                    var ss = new StreamString(pipeClient);
+                    string sendMsg, recvMsg;
+                    recvMsg = ss.ReadString();
+                    Console.WriteLine($"[Client-{clientId}({threadId}) recv] {recvMsg}");
 
-        // Remove extra characters when launched from Visual Studio
-        currentProcessName = currentProcessName.Trim('"', ' ');
-
-        currentProcessName = Path.ChangeExtension(currentProcessName, ".exe");
-        Process[] plist = new Process[numClients];
-
-        Console.WriteLine("Spawning client processes...\n");
-
-        if (currentProcessName.Contains(Environment.CurrentDirectory))
-        {
-            currentProcessName = currentProcessName.Replace(Environment.CurrentDirectory, String.Empty);
-        }
-
-        // Remove extra characters when launched from Visual Studio
-        currentProcessName = currentProcessName.Replace("\\", String.Empty);
-        currentProcessName = currentProcessName.Replace("\"", String.Empty);
-
-        int i;
-        for (i = 0; i < numClients; i++)
-        {
-            // Start 'this' program but spawn a named pipe client.
-            plist[i] = Process.Start(currentProcessName, $"spawnclient {i+1}");
-        }
-        while (i > 0)
-        {
-            for (int j = 0; j < numClients; j++)
-            {
-                if (plist[j] != null)
-                {
-                    if (plist[j].HasExited)
+                    if (recvMsg.Equals("I am the one true server!"))
                     {
-                        Console.WriteLine($"Client process[{plist[j].Id}] has exited.");
-                        plist[j] = null;
-                        i--;    // decrement the process watch count
+                        // send client info
+                        List<string> ipAddrList = Utils.LocalIpAddressList();
+                        sendMsg = $"Client-{clientId}:" + (ipAddrList.Count > 0 ? ipAddrList[0] : "");
+                        ss.WriteString(sendMsg);
+                        Console.WriteLine($"[Client-{clientId}({threadId}) send] {sendMsg}");
+
+                        // receive command from server
+                        recvMsg = ss.ReadString();
+                        string filename = "c:/textfile" + recvMsg.Split("]")[0].Split("[")[1] + ".txt";
+                        ReadFileToStream fileReader = new ReadFileToStream(ss, filename);
+                        Console.WriteLine($"[Client-{clientId}({threadId}) recv] {recvMsg}");
+
+                        // send read file
+                        fileReader.Start();
                     }
                     else
                     {
-                        Thread.Sleep(250);
+                        Console.WriteLine("Server could not be verified.");
+                    }
+                    pipeClient.Close();
+                    // Give the client process some time to display results before exiting.
+                    Thread.Sleep(4000);
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n*** Named pipe client stream with impersonation example ***\n");
+                StartClients();
+            }
+            Console.ReadLine();
+        }
+
+        // Helper function to create pipe client processes
+        private static void StartClients()
+        {
+            string currentProcessName = Environment.CommandLine;
+
+            // Remove extra characters when launched from Visual Studio
+            currentProcessName = currentProcessName.Trim('"', ' ');
+
+            currentProcessName = Path.ChangeExtension(currentProcessName, ".exe");
+            Process[] plist = new Process[numClients];
+
+            Console.WriteLine("Spawning client processes...\n");
+
+            if (currentProcessName.Contains(Environment.CurrentDirectory))
+            {
+                currentProcessName = currentProcessName.Replace(Environment.CurrentDirectory, String.Empty);
+            }
+
+            // Remove extra characters when launched from Visual Studio
+            currentProcessName = currentProcessName.Replace("\\", String.Empty);
+            currentProcessName = currentProcessName.Replace("\"", String.Empty);
+
+            int i;
+            for (i = 0; i < numClients; i++)
+            {
+                // Start 'this' program but spawn a named pipe client.
+                plist[i] = Process.Start(currentProcessName, $"spawnclient {i + 1}");
+            }
+            while (i > 0)
+            {
+                for (int j = 0; j < numClients; j++)
+                {
+                    if (plist[j] != null)
+                    {
+                        if (plist[j].HasExited)
+                        {
+                            Console.WriteLine($"Client process[{plist[j].Id}] has exited.");
+                            plist[j] = null;
+                            i--;    // decrement the process watch count
+                        }
+                        else
+                        {
+                            Thread.Sleep(250);
+                        }
                     }
                 }
             }
+            Console.WriteLine("\nClient processes finished, exiting.");
         }
-        Console.WriteLine("\nClient processes finished, exiting.");
-    }
-}
-
-// Defines the data protocol for reading and writing strings on our stream.
-public class StreamString
-{
-    private Stream ioStream;
-    private UnicodeEncoding streamEncoding;
-
-    public StreamString(Stream ioStream)
-    {
-        this.ioStream = ioStream;
-        streamEncoding = new UnicodeEncoding();
     }
 
-    public string ReadString()
+    // Defines the data protocol for reading and writing strings on our stream.
+    public class StreamString
     {
-        int len;
-        len = ioStream.ReadByte() * 256;
-        len += ioStream.ReadByte();
-        var inBuffer = new byte[len];
-        ioStream.Read(inBuffer, 0, len);
+        private Stream ioStream;
+        private UnicodeEncoding streamEncoding;
 
-        return streamEncoding.GetString(inBuffer);
-    }
-
-    public int WriteString(string outString)
-    {
-        byte[] outBuffer = streamEncoding.GetBytes(outString);
-        int len = outBuffer.Length;
-        if (len > UInt16.MaxValue)
+        public StreamString(Stream ioStream)
         {
-            len = (int)UInt16.MaxValue;
+            this.ioStream = ioStream;
+            streamEncoding = new UnicodeEncoding();
         }
-        ioStream.WriteByte((byte)(len / 256));
-        ioStream.WriteByte((byte)(len & 255));
-        ioStream.Write(outBuffer, 0, len);
-        ioStream.Flush();
 
-        return outBuffer.Length + 2;
+        public string ReadString()
+        {
+            int len = ioStream.ReadByte() * 256;
+            len += ioStream.ReadByte();
+            byte[] inBuffer = new byte[len];
+            ioStream.Read(inBuffer, 0, len);
+
+            return streamEncoding.GetString(inBuffer);
+        }
+
+        public int WriteString(string outString)
+        {
+            byte[] outBuffer = streamEncoding.GetBytes(outString);
+            int len = outBuffer.Length;
+            if (len > UInt16.MaxValue)
+            {
+                len = (int)UInt16.MaxValue;
+            }
+            ioStream.WriteByte((byte)(len / 256));
+            ioStream.WriteByte((byte)(len & 255));
+            ioStream.Write(outBuffer, 0, len);
+            ioStream.Flush();
+
+            return outBuffer.Length + 2;
+        }
+    }
+    // Contains the method executed in the context of the impersonated user
+    public class ReadFileToStream
+    {
+        private string fn;
+        private StreamString ss;
+
+        public ReadFileToStream(StreamString str, string filename)
+        {
+            fn = filename;
+            ss = str;
+        }
+
+        public void Start()
+        {
+            string contents = File.ReadAllText(fn);
+            ss.WriteString(contents);
+        }
+    }
+
+    class Utils
+    {
+        public static List<string> LocalIpAddressList()
+        {
+            List<string> ips = new List<string>();
+            IPHostEntry ipEntry = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in ipEntry.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ips.Add(ip.ToString());
+                }
+            }
+            return ips;
+        }
     }
 }
--- NamedPipeServer/NamedPipeServer.cs
+++ NamedPipeServer/NamedPipeServer.cs
@@ -4,135 +4,131 @@ using System.IO.Pipes;
 using System.Text;
 using System.Threading;
 
-public class PipeServer
+namespace TestPipe
 {
-    private static int numThreads = 4;
-
-    public static void Main()
+    public class Program
     {
-        int i;
-        Thread[] servers = new Thread[numThreads];
+        private static int numThreads = 4;
+        public static void Main()
+        {
+            int i;
+            Thread[] servers = new Thread[numThreads];
 
-        Console.WriteLine("\n*** Named pipe server stream with impersonation example ***\n");
-        Console.WriteLine("Waiting for client connect...\n");
-        for (i = 0; i < numThreads; i++)
-        {
-            servers[i] = new Thread(ServerThread);
-            servers[i].Start();
-        }
-        Thread.Sleep(250);
-        while (i > 0)
-        {
-            for (int j = 0; j < numThreads; j++)
+            Console.WriteLine("\n*** Named pipe server stream with impersonation example ***\n");
+            Console.WriteLine("Waiting for client connect...\n");
+            for (i = 0; i < numThreads; i++)
             {
-                if (servers[j] != null)
+                PipeServer pipeServer = new PipeServer();
+                servers[i] = new Thread(pipeServer.ServerThread);
+                servers[i].Start();
+            }
+            Thread.Sleep(250);
+            while (i > 0)
+            {
+                for (int j = 0; j < numThreads; j++)
                 {
-                    if (servers[j].Join(250))
+                    if (servers[j] != null)
                     {
-                        Console.WriteLine("Server thread[{0}] finished.", servers[j].ManagedThreadId);
-                        servers[j] = null;
-                        i--;    // decrement the thread watch count
+                        if (servers[j].Join(250))
+                        {
+                            Console.WriteLine("Server thread[{0}] finished.", servers[j].ManagedThreadId);
+                            servers[j] = null;
+                            i--;    // decrement the thread watch count
+                        }
                     }
                 }
             }
+            Console.WriteLine("\nServer threads exhausted, exiting.");
         }
-        Console.WriteLine("\nServer threads exhausted, exiting.");
+
     }
 
-    private static void ServerThread(object data)
+    public class PipeServer
     {
-        NamedPipeServerStream pipeServer = new NamedPipeServerStream("testpipe", PipeDirection.InOut, numThreads);
+        private int numThreads = 4;
 
-        int threadId = Thread.CurrentThread.ManagedThreadId;
-
-        // Wait for a client to connect
-        pipeServer.WaitForConnection();
-
-        Console.WriteLine("Client connected on thread[{0}].", threadId);
-        try
+        public void ServerThread(object data)
         {
-            // Read the request from the client. Once the client has
-            // written to the pipe its security token will be available.
-            StreamString ss = new StreamString(pipeServer);
+            NamedPipeServerStream pipeServer = new NamedPipeServerStream("testpipe", PipeDirection.InOut, numThreads);
 
-            // Verify our identity to the connected client using a
-            // string that the client anticipates.
-            ss.WriteString("I am the one true server!");
-            string filename = ss.ReadString();
+            int threadId = Thread.CurrentThread.ManagedThreadId;
 
-            // Read in the contents of the file while impersonating the client.
-            ReadFileToStream fileReader = new ReadFileToStream(ss, filename);
+            // Wait for a client to connect
+            pipeServer.WaitForConnection();
 
-            // Display the name of the user we are impersonating.
-            Console.WriteLine("Reading file: {0} on thread[{1}] as user: {2}.", filename, threadId, pipeServer.GetImpersonationUserName());
-            pipeServer.RunAsClient(fileReader.Start);
+            Console.WriteLine("Client connected on thread[{0}].", threadId);
+            try
+            {
+                StreamString ss = new StreamString(pipeServer);
+                string sendMsg, recvMsg;
+
+                // send server's signature string
+                sendMsg = "I am the one true server!";
+                ss.WriteString(sendMsg);
+                Console.WriteLine($"[Server({threadId}) send] {sendMsg}");
+
+                // receive client info
+                recvMsg = ss.ReadString();
+                string [] clientInfo = recvMsg.Split(":");
+                string clientId = clientInfo[0].Replace("Client-",string.Empty);
+                string clientIP = clientInfo[1];
+                Console.WriteLine($"[Server({threadId}) recv] {recvMsg}");
+
+                // send command to each client
+                sendMsg = $"Server command client[{clientId}] ro execute simulation.";
+                ss.WriteString(sendMsg);
+                Console.WriteLine($"[Server({threadId}) send] {sendMsg}");
+
+                // receive response (file content)
+                Console.WriteLine($"[Server({threadId}) recv] " + ss.ReadString());
+            }
+            // Catch the IOException that is raised if the pipe is broken
+            // or disconnected.
+            catch (IOException e)
+            {
+                Console.WriteLine("ERROR: {0}", e.Message);
+            }
+            pipeServer.Close();
         }
-        // Catch the IOException that is raised if the pipe is broken
-        // or disconnected.
-        catch (IOException e)
+    }
+
+    // Defines the data protocol for reading and writing strings on our stream
+    public class StreamString
+    {
+        private Stream ioStream;
+        private UnicodeEncoding streamEncoding;
+
+        public StreamString(Stream ioStream)
         {
-            Console.WriteLine("ERROR: {0}", e.Message);
+            this.ioStream = ioStream;
+            streamEncoding = new UnicodeEncoding();
         }
-        pipeServer.Close();
-    }
-}
 
-// Defines the data protocol for reading and writing strings on our stream
-public class StreamString
-{
-    private Stream ioStream;
-    private UnicodeEncoding streamEncoding;
-
-    public StreamString(Stream ioStream)
-    {
-        this.ioStream = ioStream;
-        streamEncoding = new UnicodeEncoding();
-    }
-
-    public string ReadString()
-    {
-        int len = 0;
-
-        len = ioStream.ReadByte() * 256;
-        len += ioStream.ReadByte();
-        byte[] inBuffer = new byte[len];
-        ioStream.Read(inBuffer, 0, len);
-
-        return streamEncoding.GetString(inBuffer);
-    }
-
-    public int WriteString(string outString)
-    {
-        byte[] outBuffer = streamEncoding.GetBytes(outString);
-        int len = outBuffer.Length;
-        if (len > UInt16.MaxValue)
+        public string ReadString()
         {
-            len = (int)UInt16.MaxValue;
+            int len = ioStream.ReadByte() * 256;
+            len += ioStream.ReadByte();
+            byte[] inBuffer = new byte[len];
+            ioStream.Read(inBuffer, 0, len);
+
+            return streamEncoding.GetString(inBuffer);
         }
-        ioStream.WriteByte((byte)(len / 256));
-        ioStream.WriteByte((byte)(len & 255));
-        ioStream.Write(outBuffer, 0, len);
-        ioStream.Flush();
 
-        return outBuffer.Length + 2;
-    }
-}
+        public int WriteString(string outString)
+        {
+            byte[] outBuffer = streamEncoding.GetBytes(outString);
+            int len = outBuffer.Length;
+            if (len > UInt16.MaxValue)
+            {
+                len = (int)UInt16.MaxValue;
+            }
+            ioStream.WriteByte((byte)(len / 256));
+            ioStream.WriteByte((byte)(len & 255));
+            ioStream.Write(outBuffer, 0, len);
+            ioStream.Flush();
 
-// Contains the method executed in the context of the impersonated user
-public class ReadFileToStream
-{
-    private string fn;
-    private StreamString ss;
-
-    public ReadFileToStream(StreamString str, string filename)
-    {
-        fn = filename;
-        ss = str;
+            return outBuffer.Length + 2;
+        }
     }
 
-    public void Start()
-    {
-        string contents = File.ReadAllText(fn);
-        ss.WriteString(contents);
-    }
 }
